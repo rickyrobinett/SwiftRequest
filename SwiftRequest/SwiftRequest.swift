c0f427950f1f5d945a7d41f84ee5ac94679252ff
Ricky Robinett <ricky.robinett@gmail.com>
2014-07-23 15:58:03 -0400

    move from convenience methods to default paramters, fix handling of Optionals

--- SwiftRequest/SwiftRequest.swift
+++ SwiftRequest/SwiftRequest.swift
@@ -15,37 +15,17 @@ class SwiftRequest {
         // we should probably be preparing something here...
     }
     
-    // Do these convenience methods even make sense? I don't think we need them
-    convenience init(url: String) {
-        self.init()
-        request(["url" : url ] )
-    }
-    
-    convenience init(url: String, callback: (err: NSError?, response: AnyObject?, body: AnyObject?)->()) {
-        self.init()
-        request(["url" : url ], callback: callback )
-    }
-    
     // GET requests
-    func get(url: String, callback: (err: NSError?, response: AnyObject?, body: AnyObject?)->()) {
-        request(["url" : url ], callback: callback )
-    }
-    
-    
-    // POST requests
-    func post(url: String, callback: (err: NSError?, response: AnyObject?, body: AnyObject?)->()) {
-        post(url, payload: Dictionary(), callback)
-    }
-    
-    func post(url: String, payload: Dictionary<String, String>, callback: (err: NSError?, response: AnyObject?, body: AnyObject?)->()) {
+    func get(url: String, auth: Dictionary<String, String> = Dictionary<String, String>(), params: Dictionary<String, String> = Dictionary<String, String>(), callback: ((err: NSError?, response: NSURLResponse?, body: AnyObject?)->())? = nil) {
         var qs = ""
-        for (key, value) in payload {
+        for (key, value) in params {
             qs += "\(key)=\(value)&"
         }
-        request(["url": url, "method" : "POST", "body" : qs] , callback)
+        request(["url" : url, "auth" : auth, "querystring": qs ], callback: callback )
     }
     
-    func post(url: String, payload: Dictionary<String, String>, auth: Dictionary<String, String>, callback: (err: NSError?, response: AnyObject?, body: AnyObject?)->()) {
+    // POST requests
+    func post(url: String, payload: Dictionary<String, String> = Dictionary<String, String>(), auth: Dictionary<String, String> = Dictionary<String, String>(), callback: ((err: NSError?, response: NSURLResponse?, body: AnyObject?)->())? = nil) {
         var qs = ""
         for (key, value) in payload {
             qs += "\(key)=\(value)&"
@@ -53,31 +33,40 @@ class SwiftRequest {
         request(["url": url, "method" : "POST", "body" : qs, "auth" : auth] , callback)
     }
     
-    
     // Actually make the requests
     func request(options: Dictionary<String, Any>, callback: ((err: NSError?, response: NSURLResponse?, body: AnyObject?)->())?) {
         if( !options["url"] ) { return }
-        var url = NSURL.URLWithString(options["url"] as String)
+        
+        var urlString = options["url"] as String
+        if( options["querystring"] && (options["querystring"] as String) != "" ) {
+            var qs = options["querystring"] as String
+            urlString = "\(urlString)?\(qs)"
+        }
+        
+        var url = NSURL.URLWithString(urlString)
         var urlRequest = NSMutableURLRequest(URL: url)
         
         if( options["method"]) {
             urlRequest.HTTPMethod = options["method"] as String
         }
         
-        if( options["body"]) {
+        if( options["body"] && options["body"] as String != "" ) {
             var postData = (options["body"] as String).dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: true)
             urlRequest.HTTPBody = postData
-            urlRequest.setValue("\(postData.length)", forHTTPHeaderField: "Content-length")
+            urlRequest.setValue("\(postData!.length)", forHTTPHeaderField: "Content-length")
         }
         
         // is there a more efficient way to do this?
-        if( options["auth"] ) {
+        if( options["auth"] && (options["auth"] as Dictionary<String,String>).count > 0) {
             var auth = options["auth"] as Dictionary<String,String>
-            var username = auth["username"]
-            var password = auth["password"]
-            var authEncoded = "\(username):\(password)".dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: true).base64Encoding()
-            var authValue = "Basic \(authEncoded)"
-            urlRequest.setValue(authValue, forHTTPHeaderField: "Authorization")
+            if( auth["username"] && auth["password"] ) {
+                var username = auth["username"]
+                var password = auth["password"]
+                var authEncoded = "\(username!):\(password!)".dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: true)!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.fromRaw(0)!)
+                println(authEncoded)
+                var authValue = "Basic \(authEncoded!)"
+                urlRequest.setValue(authValue, forHTTPHeaderField: "Authorization")
+            }
         }
         
         let task = session.dataTaskWithRequest(urlRequest, completionHandler: {body, response, err in
@@ -93,8 +82,8 @@ class SwiftRequest {
         task.resume()
     }
     
-    func request(options: Dictionary<String, Any>) {
-        request(options, callback: nil)
+    func request(url: String, callback: ((err: NSError?, response: NSURLResponse?, body: AnyObject?)->())? = nil) {
+        request(["url" : url ], callback: callback )
     }
 
 }
